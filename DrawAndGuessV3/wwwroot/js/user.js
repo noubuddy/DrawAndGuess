af639f70f3e74d1e71c2f3175d15bcbc4f1b0c0b
noubuddy <anton.nich00@gmail.com>
2022-04-17 23:23:46 +0300

    changes

--- DrawAndGuessV3/wwwroot/js/user.js
+++ DrawAndGuessV3/wwwroot/js/user.js
@@ -1,14 +1,14 @@
-var connection = new signalR.HubConnectionBuilder().withUrl("/userHub").build();
+// var connectionUser = new signalR.HubconnectionUserBuilder().withUrl("/userHub").build();
 
-connection.on('user', function (name) {
-    var name = document.getElementById("name").innerHTML;
-    var li = document.createElement("li");
-    document.getElementById("userList").appendChild(li);
-    li.textContent = `${name}`;
-});
+// connectionUser.on('user', function (name) {
+//     var name = document.getElementById("name").innerHTML;
+//     var li = document.createElement("li");
+//     document.getElementById("userList").appendChild(li);
+//     li.textContent = `${name}`;
+// });
 
-connection.start().then(function () {
-    connection.invoke("AddUser", document.getElementById("name").innerHTML);
-}).catch(function (err) {
-    return console.error(err.toString());
-});
+// connectionUser.start().then(function () {
+//     connectionUser.invoke("AddUser", document.getElementById("name").innerHTML);
+// }).catch(function (err) {
+//     return console.error(err.toString());
+// });
